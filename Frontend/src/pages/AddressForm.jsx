949cb08651f52d8e4087b220ae3513f5aaefd243
Pabitra Bhanja <pabitrabhanja2000@gmail.com>
2026-02-18 10:27:49 +0530

    Update the AddressForm page

--- Frontend/src/pages/AddressForm.jsx
+++ Frontend/src/pages/AddressForm.jsx
@@ -6,6 +6,8 @@ import { Button } from '@/components/ui/button';
 import { addAddress, deleteAddress, setSelectedAddress } from '../redux/productSlice';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function AddressForm() {
     const [formData, setFormData] = useState({
@@ -21,6 +23,7 @@ function AddressForm() {
     const { cart, addresses, selectedAddress } = useSelector(store => store.product);
     const [showForm, setShowForm] = useState(addresses.length > 0 ? false : true);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -44,8 +47,102 @@ function AddressForm() {
 
     console.log(cart);
 
-    
-    
+    const handlePayment = async () => {
+        const accessToken = localStorage.getItem('token');
+        try {
+            const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/api/orders/create-order`, {
+                products: cart?.items?.map(item => ({
+                    productId: item.productId._id,
+                    quantity: item.quantity,
+                })),
+                tax: tax,
+                amount: total,
+                shipping: shipping,
+                currency: "INR"
+            }, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            if (!data.success) return toast.error("Something went wrong");
+
+            const options = {
+                key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+                amount: data.order.amount,
+                currency: data.order.currency,
+                order_id: data.order.id,
+                name: "E-shop",
+                description: "Payment for order",
+                handler: async function (response) {
+                    try {
+                        const verifyres = await axios.post(`${import.meta.env.VITE_API_URL}/api/orders/verify-payment`, response, {
+                            headers: {
+                                Authorization: `Bearer ${accessToken}`
+                            }
+                        });
+
+                        if (verifyres.data.success) {
+                            toast.success("✅ Payment successfull !");
+                            dispatch(setCart({
+                                items: [],
+                                totalPrice: 0
+                            }));
+                            navigate("/order-success");
+                        } else {
+                            toast.error("❌ Payment verification failed !");
+                        }
+
+
+                    } catch (error) {
+                        toast.error("Error verifying Payment");
+                    }
+                },
+                modal: {
+                    ondismiss: async () => {
+                        //Handle user closing the popup
+                        await axios.post(`${import.meta.env.VITE_API_URL}/api/orders/verify-payment`, {
+                            razorpay_order_id: data.order.id,
+                            paymentFailed: true
+                        }, {
+                            headers: {
+                                Authorization: `Bearer ${accessToken}`
+                            }
+                        })
+                        toast.error("❌ Payment verification failed !");
+                    }
+                },
+                prefill: {
+                    name: formData.fullName,
+                    email: formData.email,
+                    contact: formData.phone,
+                },
+                theme: {
+                    color: "#F47286"
+                }
+            }
+            const rzp = new window.Razorpay(options);
+
+
+            //Listen for payment failure
+            rzp.on('payment.failed', async function (response) {
+                await axios.post(`${import.meta.env.VITE_API_URL}/api/orders/verify-payment`, {
+                    razorpay_order_id: data.order.id,
+                    paymentFailed: true
+                }, {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`
+                    }
+                })
+                toast.error("Payment failed. Please try again");
+            });
+            rzp.open();
+
+        } catch (error) {
+            console.log(error);
+            toast.error("Something went wrong while Processing payment");
+        }
+    }
 
     return (
         <div className='max-w-7xl mx-auto p-10'>
@@ -173,10 +270,13 @@ function AddressForm() {
 
                                 <Button type="button" onClick={() => setShowForm(true)} variant='outline' className='w-full mt-4'>+ Add New Address</Button>
                                 <Button
+                                    onClick={handlePayment}
                                     disabled={selectedAddress === null}
                                     type="button"
-                                    variant='outline' className='w-full mt-4 bg-purple-600 text-white hover:bg-purple-700'>Proceed to Checkout</Button>
-                            </div>
+                                    variant='outline' className='w-full mt-4 bg-purple-600 text-white hover:bg-purple-700'>
+                                    Proceed to Checkout
+                                </Button>
+                            </div> 
                         )
                     }
                 </div>
